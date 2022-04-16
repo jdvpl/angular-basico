10bf6d5cddd5782cc675b926c0a5f9bdabec5dce
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-04-15 22:02:52 -0500

    ngIf mostar y ocular un listado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'kaka';
-  curso="Angular";
+  title = 'Bienvenido al curso de Angular-Spring';
 }
--- src/app/footer/directiva/directiva.component.ts
+++ src/app/footer/directiva/directiva.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectivaComponent {
   listaCurso:string[]=["TS","Java","php","C#","Javascript"]
+  enable=true;
   constructor() { }
-
+  
+  setEnable():void{
+    this.enable=(this.enable==true)?false:true;
+  }
 }
